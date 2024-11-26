b29ee538869c90f662b5612519943625063e360c
MAIN6419 <dark9737@gmail.com>
2024-11-26 19:26:28 +0900

    Init: 초기 세팅

---
+++ webpack.common.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: "./index.ts",
+  output: {
+    filename: "popup.js",
+    path: path.resolve(__dirname, "dist"),
+    chunkFilename: "[name].bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: {
+          loader: "file-loader",
+          options: { outputPath: "./src/public/icons", name: "[name].[ext]" },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "src/styles.css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/public/icons"),
+          to: "src/public/icons",
+        },
+      ],
+    }),
+    new BundleAnalyzerPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+};
---
+++ webpack.dev.js
@@ -0,0 +1,9 @@
+// webpack.dev.js
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-cheap-module-source-map',
+  devServer: { contentBase: './dist' },
+});
