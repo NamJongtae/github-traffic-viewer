3de4816068e05f64ad5a7a5143cbaf290ac12b6d
MAIN6419 <dark9737@gmail.com>
2024-11-28 17:16:40 +0900

    Feat: 폼 에러 메세지 생성 유틸 함수 추가

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,15 @@ export function $(selector: string) {
   const rootEl = document.querySelector("#app") as HTMLDivElement;
   return rootEl.querySelector(selector);
 }
+
+// 에러 메시지 생성
+export function generateFormErrorMsg(error: Error) {
+  let message = "Something went wrong. Please try again later.";
+  if (error.message.includes("401")) {
+    message = "Please check your Access Token.";
+  } else if (error.message.includes("404")) {
+    message = "Please check your GitHub username or repository name.";
+  }
+
+  return message;
+}
