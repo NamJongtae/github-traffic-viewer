6a02431720b24f83416aed35018725676b45bee0
MAIN6419 <dark9737@gmail.com>
2024-11-28 18:29:36 +0900

    Feat: Model 인스턴스 생성 및 Controller 인스턴스 생성

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
-import { View } from "./src/view/View";
 import "./src/styles.css";
+import { View } from "./src/view/View";
+import { Controller } from "./src/controller/Controller";
+import { TrafficDataModel } from "./src/model/TrafficDataModel";
+import { LocalStorageModel } from "./src/model/LocalStorageModel";
+
+const trafficDataModel = new TrafficDataModel();
+const localStorageModel = new LocalStorageModel();
 
 const view = new View();
-document.addEventListener("DOMContentLoaded", () => view.init());
+new Controller(trafficDataModel, localStorageModel, view);
