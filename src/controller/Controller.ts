b86e1102c938db57acf416a8802a813be26efc79
MAIN6419 <dark9737@gmail.com>
2024-11-28 18:27:52 +0900

    Feat: Model 매개변수 추가 및 getTrafficForm, loadTrafficForm 바인딩 메서드 추가

--- src/controller/Controller.ts
+++ src/controller/Controller.ts
@@ -1,9 +1,15 @@
-import { $ } from "../utils";
+import { LocalStorageModel } from "../model/LocalStorageModel";
+import { TrafficDataModel } from "../model/TrafficDataModel";
+import { $, generateFormErrorMsg } from "../utils";
 import { View } from "../view/View";
 
 export class Controller {
-  constructor(private view: View) {
-    this.view.init(()=>this.bindMainMenuEvents());
+  constructor(
+    private trafficDataModel: TrafficDataModel,
+    private localStorageModel: LocalStorageModel,
+    private view: View
+  ) {
+    this.view.init(() => this.bindMainMenuEvents());
   }
 
   bindMainMenuEvents() {
@@ -24,11 +30,11 @@ export class Controller {
   }
 
   private handleClickGetTraffic() {
-    // 버튼 클릭 시 get-traffic-form 렌더링
+    this.view.renderGetTrafficForm(() => this.bindGetTrafficFormEvents());
   }
 
   private handleClickLoadTraffic() {
-    // 버튼 클릭 시 load-traffic-form 렌더링
+    this.view.renderLoadTrafficForm(() => this.bindLoadTrafficFormEvents());
   }
 
   private handleClickDocsBtn() {
@@ -37,5 +43,132 @@ export class Controller {
       "Docs",
       "width=1000px,height=1000px,scrollbars=yes"
     );
+  }
+
+  bindGetTrafficFormEvents() {
+    const form = $(".get-traffic-form") as HTMLFormElement;
+    this.view.bindEvent(form, "submit", (e) => this.handleSubmitTrafficForm(e));
+
+    const inputs = document.querySelectorAll(
+      ".input-group input"
+    ) as NodeListOf<HTMLInputElement>;
+    this.view.bindEvents(inputs, "input", (event) => {
+      const input = event.target as HTMLInputElement;
+      const clearBtn = input.nextElementSibling as HTMLButtonElement;
+      this.toggleClearButton(input, backBtn);
+      if (clearBtn && input instanceof HTMLInputElement) {
+        this.bindInputClearEvents(input, clearBtn);
+      }
+    });
+
+    inputs.forEach((input) => {
+      const clearBtn = input.nextElementSibling as HTMLButtonElement;
+      this.toggleClearButton(input, clearBtn);
+    });
+
+    const backBtn = $(".back-btn") as HTMLButtonElement;
+    this.view.bindEvent(backBtn, "click", () => this.handleClickBackButton());
+  }
+
+  private async handleSubmitTrafficForm(event: Event) {
+    event.preventDefault();
+
+    const githubId = ($("#github-username") as HTMLInputElement).value;
+    const githubToken = ($("#github-token") as HTMLInputElement).value;
+    const repoName = ($("#repo-name") as HTMLInputElement).value;
+
+    this.view.activeFormLoading();
+
+    try {
+      const responseData = await this.trafficDataModel.fetchTrafficData(
+        githubId,
+        repoName,
+        githubToken
+      );
+
+      // 로컬 스토리지와 병합
+      const mergedData = await this.localStorageModel.mergeDataWithLocalStorage(
+        repoName,
+        responseData.views
+      );
+
+      this.trafficDataModel.setTrafficData(mergedData);
+
+      // Result  element 렌더링
+      //...
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        const message = generateFormErrorMsg(error);
+        this.view.renderErrorMsg(".back-btn", message);
+      }
+    } finally {
+      this.view.inactiveFormLoading();
+    }
+  }
+
+  private toggleClearButton(
+    input: HTMLInputElement,
+    button: HTMLButtonElement
+  ) {
+    button.style.display = input.value ? "block" : "none";
+  }
+
+  private bindInputClearEvents(
+    input: HTMLInputElement,
+    clearBtn: HTMLButtonElement
+  ) {
+    // 초기 버튼 상태 토글
+    this.toggleClearButton(input, clearBtn);
+
+    this.view.bindEvent(input, "input", () =>
+      this.toggleClearButton(input, clearBtn)
+    );
+
+    this.view.bindEvent(clearBtn, "click", () => {
+      input.value = "";
+      this.toggleClearButton(input, clearBtn);
+    });
+  }
+
+  private handleClickBackButton() {
+    this.view.removeElement(".get-traffic-form");
+    this.view.removeElement(".load-traffic-form");
+    this.view.removeElement(".result");
+    this.view.renderMainMenu(() => this.bindMainMenuEvents());
+  }
+
+  bindLoadTrafficFormEvents() {
+    const form = $(".load-traffic-form") as HTMLFormElement;
+    this.view.bindEvent(form, "submit", this.handleSubmitLoadForm.bind(this));
+
+    const input = $(".input-group input") as HTMLInputElement;
+    const clearBtn = input.nextElementSibling as HTMLButtonElement;
+    this.toggleClearButton(input, clearBtn);
+
+    this.view.bindEvent(input, "input", () => {
+      this.bindInputClearEvents(input, clearBtn);
+    });
+
+    const backBtn = $(".back-btn") as HTMLButtonElement;
+    this.view.bindEvent(
+      backBtn,
+      "click",
+      this.handleClickBackButton.bind(this)
+    );
+  }
+
+  private async handleSubmitLoadForm(event: Event) {
+    event.preventDefault();
+
+    const repoName = ($("#repo-name") as HTMLInputElement).value;
+    const data = await this.localStorageModel.loadFromLocalStorage(repoName);
+
+    if (data.length === 0) {
+      this.view.renderErrorMsg(
+        ".back-btn",
+        "The saved traffic data for this repository does not exist."
+      );
+      return;
+    }
   }
 }
