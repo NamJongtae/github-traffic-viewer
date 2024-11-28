fb75d5f647338c53a87a9f737710f0c81070cd2b
MAIN6419 <dark9737@gmail.com>
2024-11-28 17:12:58 +0900

    Feat: getTrafficForm 및 LoadTafficForm 렌더링 메서드 추가 - 에러 메세지 생성 및 삭제 메서드 추가 - form 로딩 적용 및 로딩 해제 메서드 추가 - element 제거 메서드 추가

--- src/view/View.ts
+++ src/view/View.ts
@@ -1,20 +1,65 @@
-import { layout, mainMenu } from "../template";
+import { getTrafficForm, layout, loadTrafficForm, mainMenu } from "../template";
 import { $ } from "../utils";
 
 const rootEl = document.querySelector("#app")!;
 
 export class View {
-  init() {
+  init(bindEvents: () => void) {
     this.renderLayout();
-    this.renderMainMenu();
+    this.renderMainMenu(() => bindEvents());
   }
 
   renderLayout() {
     rootEl.innerHTML = layout;
   }
 
-  renderMainMenu() {
+  renderMainMenu(bindEvents: () => void) {
     $(".title")!.insertAdjacentHTML("afterend", mainMenu);
+    bindEvents();
+  }
+
+  renderGetTrafficForm(bindEvents: () => void) {
+    this.removeElement(".main-menu");
+    $(".title")!.insertAdjacentHTML("afterend", getTrafficForm);
+    bindEvents();
+  }
+
+  renderLoadTrafficForm(bindEvents: () => void) {
+    this.removeElement(".main-menu");
+    $(".title")!.insertAdjacentHTML("afterend", loadTrafficForm);
+    bindEvents();
+  }
+
+  renderErrorMsg(targetSelector: string, message: string) {
+    const errorEl = $(".error-msg") as HTMLParagraphElement;
+    if (errorEl) {
+      errorEl.innerText = message;
+    } else {
+      $(targetSelector)!.insertAdjacentHTML(
+        "afterend",
+        `<p class="error-msg"}>${message}</p>`
+      );
+    }
+  }
+
+  clearErrorMsg(type: "form" | "filter" = "form") {
+    this.removeElement(
+      `${type === "filter" ? ".filter-error-msg" : "error-msg"}`
+    );
+  }
+
+  activeFormLoading() {
+    const submitBtnEl = $(".submit-btn") as HTMLButtonElement;
+    submitBtnEl.textContent = "Loading Traffic Data...";
+    submitBtnEl.style.opacity = "0.5";
+    submitBtnEl.style.pointerEvents = "none";
+  }
+
+  inactiveFormLoading() {
+    const submitBtnEl = $(".submit-btn") as HTMLButtonElement;
+    submitBtnEl.textContent = "Get Traffic Data";
+    submitBtnEl.style.opacity = "1";
+    submitBtnEl.style.pointerEvents = "auto";
   }
 
   // 이벤트 바인딩 메서드
@@ -34,5 +79,9 @@ export class View {
     callback: EventListener
   ) {
     elements.forEach((element) => this.bindEvent(element, event, callback));
+  }
+
+  removeElement(selector: string) {
+    $(selector)?.remove();
   }
 }
