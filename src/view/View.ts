7f59b7410160a8ed6003fa539800d52937f1848c
MAIN6419 <dark9737@gmail.com>
2024-11-28 14:05:48 +0900

    Feat: View 이벤트 바인딩 메서드 추가

--- src/view/View.ts
+++ src/view/View.ts
@@ -1,5 +1,5 @@
-import { layout, mainMenu } from '../template';
-import { $ } from '../utils';
+import { layout, mainMenu } from "../template";
+import { $ } from "../utils";
 
 const rootEl = document.querySelector("#app")!;
 
@@ -15,5 +15,24 @@ export class View {
 
   renderMainMenu() {
     $(".title")!.insertAdjacentHTML("afterend", mainMenu);
+  }
+
+  // 이벤트 바인딩 메서드
+  bindEvent<T extends HTMLElement>(
+    element: T | null,
+    event: string,
+    callback: EventListener
+  ) {
+    if (element) {
+      element.addEventListener(event, callback);
+    }
+  }
+
+  bindEvents<T extends HTMLElement>(
+    elements: NodeListOf<T>,
+    event: string,
+    callback: EventListener
+  ) {
+    elements.forEach((element) => this.bindEvent(element, event, callback));
   }
 }
