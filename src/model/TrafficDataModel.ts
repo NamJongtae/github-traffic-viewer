51d0dbaa67fd4f5604f456a0d9e87326c7a32f90
MAIN6419 <dark9737@gmail.com>
2024-11-29 17:14:42 +0900

    Feat: 데이터 필터링 및 정렬 관련 메서드 추가 - 데이터를 날짜별로 필터링 하는 filterDataByDate 메서드 추가 - 데이터를 정렬 기준으로 정렬하는 sortData 메서드 추가 - 정렬 및 날짜 필터링을 함께 하는 getFilteredAndSotredData 메서드 추가 - 필터링한 데이터 총합을 계산하는 calculateTotalTraffic 메서드 추가

--- src/model/TrafficDataModel.ts
+++ src/model/TrafficDataModel.ts
@@ -1,4 +1,4 @@
-import { ResponseTrafficData, TrafficData } from '../types/trafficDataTypes';
+import { ResponseTrafficData, TrafficData } from "../types/trafficDataTypes";
 
 export class TrafficDataModel {
   data: TrafficData;
@@ -33,5 +33,58 @@ export class TrafficDataModel {
     }
 
     return (await response.json()) as ResponseTrafficData;
+  }
+
+  filterDataByDate(startDate: string, endDate: string) {
+    const start = startDate ? new Date(startDate) : null;
+    const end = endDate ? new Date(endDate) : null;
+
+    return this.data.filter((item) => {
+      const itemDate = new Date(item.timestamp);
+      return (!start || itemDate >= start) && (!end || itemDate <= end);
+    });
+  }
+
+  sortData(filteredData: TrafficData, sortOrder: string) {
+    const sortedData = filteredData.sort((a, b) => {
+      if (sortOrder === "views") {
+        // views 필드 기준 정렬
+        return b.count - a.count;
+      } else if (sortOrder === "visitors") {
+        // uniques 필드 기준 정렬
+        return b.uniques - a.uniques;
+      } else {
+        // 기본 날짜 정렬
+        const dateA = new Date(a.timestamp).getTime();
+        const dateB = new Date(b.timestamp).getTime();
+        return sortOrder === "asc" ? dateA - dateB : dateB - dateA;
+      }
+    });
+    return sortedData;
+  }
+
+  getFilteredAndSortedData(
+    startDate: string,
+    endDate: string,
+    sortOrder: string
+  ): TrafficData {
+    const start = startDate ? new Date(startDate) : null;
+    const end = endDate ? new Date(endDate) : null;
+
+    // 데이터 필터링
+    const filteredData = this.data.filter((item) => {
+      const itemDate = new Date(item.timestamp);
+      return (!start || itemDate >= start) && (!end || itemDate <= end);
+    });
+
+    // 데이터 정렬
+    const sortedData = this.sortData(filteredData, sortOrder);
+    return sortedData;
+  }
+
+  calculateTotalTraffic(data: TrafficData) {
+    const views = data.reduce((sum, item) => sum + item.count, 0);
+    const visitors = data.reduce((sum, item) => sum + item.uniques, 0);
+    return { views, visitors };
   }
 }
