ac031723fe8146502953c213537fd1a191a0c039
MAIN6419 <dark9737@gmail.com>
2024-11-30 16:15:54 +0900

    Feat: JSON 및 EXCEL 다운로드 유틸 함수 생성

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import { TrafficData } from "./types/trafficDataTypes";
+import * as XLSX from "xlsx";
+
 // DOM element selector
 export function $(selector: string) {
   const rootEl = document.querySelector("#app") as HTMLDivElement;
@@ -27,12 +30,63 @@ export function formatDate(dateString: string) {
 }
 
 // 날짜 필터 유효성 체크
-export function validateDates(
-  startDate: string,
-  endDate: string
-) {
+export function validateDates(startDate: string, endDate: string) {
   if (startDate && endDate && new Date(startDate) > new Date(endDate)) {
     return false;
   }
   return true;
+}
+
+export function downloadJson(data: TrafficData) {
+  if (!data || data.length === 0) {
+    alert("The data to download does not exist.");
+    return;
+  }
+
+  const transformData = data.map((item) => ({
+    Date: item.timestamp,
+    views: item.count,
+    visitors: item.uniques,
+  }));
+
+  const blob = new Blob([JSON.stringify(transformData, null, 2)], {
+    type: "application/json",
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `trafficData.json`;
+  a.click();
+  URL.revokeObjectURL(url);
+}
+
+export function downloadExcel(data: TrafficData) {
+  if (!data || data.length === 0) {
+    alert("The data to download does not exist.");
+    return;
+  }
+
+  const transformData = data.map((item) => ({
+    Date: item.timestamp,
+    views: item.count,
+    visitors: item.uniques,
+  }));
+
+  const worksheet = XLSX.utils.json_to_sheet(transformData);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Traffic Data");
+
+  const excelBuffer = XLSX.write(workbook, {
+    bookType: "xlsx",
+    type: "array",
+  });
+  const blob = new Blob([excelBuffer], {
+    type: "application/octet-stream",
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = `trafficData.xlsx`;
+  a.click();
+  URL.revokeObjectURL(url);
 }
