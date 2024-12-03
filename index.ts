f7d53f949556324b3a5987a7ea148f5390fde901
MAIN6419 <dark9737@gmail.com>
2024-12-03 16:48:44 +0900

    Fix: 필요한 MVC 인스턴스 생성 및 순환 참조 문제를 해결하기 위해 setDependecy 호출

--- index.ts
+++ index.ts
@@ -1,11 +1,32 @@
 import "./src/styles.css";
-import { View } from "./src/view/View";
-import { Controller } from "./src/controller/Controller";
-import { TrafficDataModel } from "./src/model/TrafficDataModel";
+import { FormController } from "./src/controller/FormController";
+import { MainMenuController } from "./src/controller/MainMenuController";
+import { ResultController } from "./src/controller/ResultController";
 import { LocalStorageModel } from "./src/model/LocalStorageModel";
+import { TrafficDataModel } from "./src/model/TrafficDataModel";
+import { FormView } from "./src/view/FormView";
+import { MainMenuView } from "./src/view/MainMenuView";
+import { ResultView } from "./src/view/ResultView";
 
+// View Instance
+const mainMenuView = new MainMenuView();
+const formView = new FormView();
+const resultView = new ResultView();
+
+// Model Instance
 const trafficDataModel = new TrafficDataModel();
 const localStorageModel = new LocalStorageModel();
 
-const view = new View();
-new Controller(trafficDataModel, localStorageModel, view);
+// Controller Instance
+const resultController = new ResultController(trafficDataModel, resultView);
+const mainMenuController = new MainMenuController(mainMenuView);
+const formController = new FormController(
+  trafficDataModel,
+  localStorageModel,
+  formView,
+  resultController
+);
+
+// To resolve circular dependencies.
+mainMenuController.setDependency(formController);
+formController.setDependency(mainMenuController);
