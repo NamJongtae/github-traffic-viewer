90e916c8ebe7e6ee49c1a4e390bdab196fe81f64
MAIN6419 <dark9737@gmail.com>
2024-12-04 15:34:44 +0900

    Fix: render 메서드명 수정 및 toggleClearButton 메서드 추가 - 메서드명 일관성 유지를 위해 render 메서드명 수정 - toggleClearButton FormController에서 제거하고 FormView에 메서드 추가

--- src/view/FormView.ts
+++ src/view/FormView.ts
@@ -1,29 +1,33 @@
-import { BaseView } from './BaseView';
+import { BaseView } from "./BaseView";
 import { getTrafficForm, loadTrafficForm } from "../template";
 import { $ } from "../utils";
 
 export class FormView extends BaseView {
-  renderTrafficForm(bindEvents: () => void) {
+  renderGetTrafficForm(bindEvents: () => void) {
     this.removeElement(".main-menu");
     $(".title")!.insertAdjacentHTML("afterend", getTrafficForm);
     bindEvents();
   }
 
-  renderLoadStorageTrafficForm(bindEvents: () => void) {
+  renderLoadTrafficForm(bindEvents: () => void) {
     this.removeElement(".main-menu");
     $(".title")!.insertAdjacentHTML("afterend", loadTrafficForm);
     bindEvents();
   }
 
+  toggleClearButton(input: HTMLInputElement, button: HTMLButtonElement) {
+    button.style.display = input.value ? "block" : "none";
+  }
+
   activeFormLoading() {
-    const submitBtnEl = document.querySelector(".submit-btn") as HTMLButtonElement;
+    const submitBtnEl = $(".submit-btn") as HTMLButtonElement;
     submitBtnEl.textContent = "Loading Traffic Data...";
     submitBtnEl.style.opacity = "0.5";
     submitBtnEl.style.pointerEvents = "none";
   }
 
   inactiveFormLoading() {
-    const submitBtnEl = document.querySelector(".submit-btn") as HTMLButtonElement;
+    const submitBtnEl = $(".submit-btn") as HTMLButtonElement;
     submitBtnEl.textContent = "Get Traffic Data";
     submitBtnEl.style.opacity = "1";
     submitBtnEl.style.pointerEvents = "auto";
