3e869a8b8b2c6b53a063132325f5f2fb89e0884e
MAIN6419 <dark9737@gmail.com>
2024-12-04 23:13:08 +0900

    Feat: createRepoListOption 메서드 추가 - repository selecotr의 옵션들을 생성하는 메서드 추가

--- src/view/FormView.ts
+++ src/view/FormView.ts
@@ -15,6 +15,26 @@ export class FormView extends BaseView {
     bindEvents();
   }
 
+  createRepoListOption(repoList: { lastUpdated: string; repoName: string }[]) {
+    const repoSelector = $("#repo-selector") as HTMLSelectElement;
+
+    if (repoList.length > 0) {
+      repoList.forEach((data) => {
+        const option = document.createElement("option");
+        option.value = data.repoName;
+        option.textContent = data.repoName;
+        option.dataset.lastupdated = data.lastUpdated;
+        repoSelector.appendChild(option);
+      });
+    } else {
+      const noDataOption = document.createElement("option");
+      noDataOption.value = "";
+      noDataOption.textContent = "No repositories found";
+      noDataOption.disabled = true;
+      repoSelector.appendChild(noDataOption);
+    }
+  }
+
   toggleClearButton(input: HTMLInputElement, button: HTMLButtonElement) {
     button.style.display = input.value ? "block" : "none";
   }
