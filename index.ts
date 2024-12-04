1f72894f60fc84ce07d3667ebd400b8c616b48ce
MAIN6419 <dark9737@gmail.com>
2024-12-04 16:07:02 +0900

    Fix: setDependency 메서드 호출 제거 eventBus 인스턴스 생성 및 Controller에 전달 - 순환 참조 의존성 제거를 위해 사용하던 setDependency 메서드 호출 제거 - eventBus를 Controller에 전달하여 순환 참조 의존성 제거

--- index.ts
+++ index.ts
@@ -1,32 +1,27 @@
-import "./src/styles.css";
 import { FormController } from "./src/controller/FormController";
 import { MainMenuController } from "./src/controller/MainMenuController";
 import { ResultController } from "./src/controller/ResultController";
+import { EventBus } from "./src/EventBus";
 import { LocalStorageModel } from "./src/model/LocalStorageModel";
 import { TrafficDataModel } from "./src/model/TrafficDataModel";
+import "./src/styles.css";
 import { FormView } from "./src/view/FormView";
 import { MainMenuView } from "./src/view/MainMenuView";
 import { ResultView } from "./src/view/ResultView";
+
+
+// Model Instance
+const trafficDataModel = new TrafficDataModel();
+const localStorageModel = new LocalStorageModel();
 
 // View Instance
 const mainMenuView = new MainMenuView();
 const formView = new FormView();
 const resultView = new ResultView();
 
-// Model Instance
-const trafficDataModel = new TrafficDataModel();
-const localStorageModel = new LocalStorageModel();
+const eventBus = new EventBus();
 
 // Controller Instance
-const resultController = new ResultController(trafficDataModel, resultView);
-const mainMenuController = new MainMenuController(mainMenuView);
-const formController = new FormController(
-  trafficDataModel,
-  localStorageModel,
-  formView,
-  resultController
-);
-
-// To resolve circular dependencies.
-mainMenuController.setDependency(formController);
-formController.setDependency(mainMenuController);
+new MainMenuController(mainMenuView, eventBus);
+new FormController(trafficDataModel, localStorageModel, formView, eventBus);
+new ResultController(trafficDataModel, resultView, eventBus);
