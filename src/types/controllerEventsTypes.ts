efe3e0c56b364e61c1c7f598ce863a5fae456530
MAIN6419 <dark9737@gmail.com>
2024-12-05 16:35:02 +0900

    Fix: ResultControllerEvents 타입 initalizeResult repoName 파라미터 추가

--- src/types/controllerEventsTypes.ts
+++ src/types/controllerEventsTypes.ts
@@ -10,5 +10,5 @@ export type FormControllerEvents = {
 };
 
 export type ResultControllerEvents = {
-  initializeResult: (data: TrafficData[]) => void;
+  initializeResult: (data: TrafficData[], repoName: string) => void;
 };
